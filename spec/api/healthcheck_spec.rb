7ae75a27c99732055bd2bdeb3ba343431ef8c814
Martin Anderson <46332+martinjanderson@users.noreply.github.com>
2023-03-25 22:19:40 -0700

    Copilot: Add API specs

--- spec/api/healthcheck_spec.rb
+++
@@ -1,9 +0,0 @@
-# spec/api/healthcheck_spec.rb
-require './spec/spec_helper'
-
-describe 'Healthcheck Endpoint' do
-  it 'returns ok' do
-    get '/healthcheck'
-    expect(last_response).to be_ok
-  end
-end
